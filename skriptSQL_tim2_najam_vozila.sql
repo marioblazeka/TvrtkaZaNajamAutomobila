3a1f82bc6f3d33d6e6835daa717b53ce5a65b14c
Mario Blažeka <147752707+marioblazeka@users.noreply.github.com>
2024-05-07 11:31:41 +0200

    Add files via upload

---
+++ skriptSQL_tim2_najam_vozila.sql
@@ -0,0 +1,375 @@
+CREATE DATABASE najam_vozila;
+
+USE najam_vozila;
+
+CREATE TABLE klijent (
+    id INT NOT NULL,
+    ime VARCHAR (30), 
+    prezime VARCHAR (30), 
+    identifikacijski_broj VARCHAR (13), 
+    id_kontakt_klijent INT, 
+    id_pravna_osoba INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE zaposlenik (
+    id MEDIUMINT NOT NULL, 
+    id_nadredeni_zaposlenik MEDIUMINT, 
+    ime VARCHAR (30), 
+    prezime VARCHAR (30), 
+    identifikacijski_broj VARCHAR (15),
+    drzava_radno_mjesto VARCHAR (47),
+    grad_radno_mjesto VARCHAR (100),
+    adresa_radno_mjesto VARCHAR (100),
+    spol CHAR (1),
+    broj_telefona VARCHAR (20),
+    broj_mobitela VARCHAR (20),
+    email VARCHAR (320),
+    id_zanimanje SMALLINT, 
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_zanimanje) REFERENCES zanimanje (id)
+);
+
+CREATE TABLE zanimanje (
+    id SMALLINT NOT NULL,
+    opis_zanimanja TEXT,
+    odjel VARCHAR (50),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE kontakt_klijenta (
+    id INT NOT NULL, 
+    email VARCHAR (320), 
+    broj_mobitela VARCHAR (20), 
+    broj_telefona VARCHAR (20),
+    id_klijent INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_klijent) REFERENCES klijent (id)
+);
+
+CREATE TABLE prihod (
+    id TINYINT NOT NULL, 
+    opis TEXT,
+    tip_prihoda VARCHAR (25),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE prihod_za_zaposlenika (
+    id INT NOT NULL,
+    datum DATE,
+    id_zaposlenik MEDIUMINT, 
+    id_transakcija_prihoda INT,
+    id_prihod TINYINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_zaposlenik) REFERENCES zaposlenik (id),
+    FOREIGN KEY (id_transakcija_prihoda) REFERENCES transakcija (id),
+    FOREIGN KEY (id_prihod) REFERENCES prihod (id)
+);
+
+CREATE TABLE popust (
+    id TINYINT NOT NULL, 
+    tip_popusta VARCHAR (40),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE popust_za_klijenta (
+    id INT NOT NULL,
+    datum_primitka DATE, 
+    datum_koristenja DATE,
+    status VARCHAR (10), 
+    id_klijent INT,
+    id_popust TINYINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_klijent) REFERENCES klijent (id),
+    FOREIGN KEY (id_popust) REFERENCES popust (id)
+);
+
+CREATE TABLE poslovni_trosak (
+    id INT NOT NULL, 
+    id_transakcija_poslovnog_troska BIGINT, 
+    svrha VARCHAR (40), 
+    opis TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_transakcija_poslovnog_troska) REFERENCES transakcija (id)
+);
+
+CREATE TABLE transakcija (
+    id BIGINT NOT NULL, 
+    datum DATE, 
+    iznos NUMERIC (12,2), 
+    broj_racuna VARCHAR (7), 
+    placeno NUMERIC (12,2),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE gotovinsko_placanje (
+    id BIGINT NOT NULL, 
+    id_transakcija_gotovina BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_transakcija_gotovina) REFERENCES transakcija (id)
+);
+
+CREATE TABLE karticno_placanje (
+    id BIGINT NOT NULL, 
+    tip_kartice VARCHAR (20), 
+    id_pravna_osoba_banka MEDIUMINT, 
+    id_transakcija_kartica BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_pravna_osoba_banka) REFERENCES pravna_osoba (id),
+    FOREIGN KEY (id_transakcija_kartica) REFERENCES transakcija (id)
+);
+
+CREATE TABLE kriptovalutno_placanje (
+    id BIGINT NOT NULL, 
+    kriptovaluta VARCHAR (50), 
+    broj_kripto_novcanika VARCHAR (60), 
+    id_transakcija_kripto BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_transakcija_kripto) REFERENCES transakcija (id)
+);
+
+CREATE TABLE pravna_osoba (
+    id MEDIUMINT NOT NULL, 
+    ime VARCHAR (100), 
+    identifikacijski_broj VARCHAR (25),
+    id_kontakt_pravna_osoba INT,
+    drzava_sjediste VARCHAR (47),
+    grad_sjediste VARCHAR (100),
+    adresa_sjediste VARCHAR (100),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE kontakt_pravne_osobe (
+    id INT NOT NULL, 
+    email VARCHAR (320), 
+    broj_mobitela VARCHAR (20), 
+    broj_telefona VARCHAR (20),
+    opis VARCHAR (100),
+    id_pravna_osoba MEDIUMINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_pravna_osoba) REFERENCES pravna_osoba (id)
+);
+
+CREATE TABLE najam_vozila (
+    id BIGINT NOT NULL, 
+    id_transakcija_najam BIGINT, 
+    id_klijent_najam INT, 
+    id_zaposlenik_najam MEDIUMINT, 
+    datum_pocetka DATE, 
+    datum_zavrsetka DATE, 
+    status VARCHAR (15),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_transakcija_najam) REFERENCES transakcija (id),
+    FOREIGN KEY (id_klijent_najam) REFERENCES klijent (id),
+    FOREIGN KEY (id_zaposlenik_najam) REFERENCES zaposlenik (id)
+);
+
+CREATE TABLE vozilo (
+    id INT NOT NULL,
+    godina_proizvodnje CHAR(4), 
+    registracijska_tablica VARCHAR (15), 
+    tip_punjenja VARCHAR (10),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE vozilo_na_najmu (
+    id BIGINT NOT NULL,
+    pocetna_kilometraza NUMERIC (10, 2), 
+    zavrsna_kilometraza NUMERIC (10, 2), 
+    id_najam BIGINT,
+    id_vozilo INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_najam) REFERENCES najam_vozila (id),
+    FOREIGN KEY (id_vozilo) REFERENCES vozilo (id)
+);
+
+CREATE TABLE automobil (
+    id MEDIUMINT NOT NULL, 
+    id_slika_automobila INT,
+    id_serija_auto_kamion SMALLINT,
+    id_vozilo INT,
+    duljina NUMERIC (3, 2),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_serija_auto_kamion) REFERENCES serija_auto_kamion (id),
+    FOREIGN KEY (id_vozilo) REFERENCES vozilo (id)
+);
+
+CREATE TABLE motocikl (
+    id MEDIUMINT NOT NULL, 
+    id_slika_motocikla INT,
+    id_serija_motocikl SMALLINT,
+    id_vozilo INT,
+    duljina NUMERIC (3, 2),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_serija_motocikl) REFERENCES serija_motocikl (id),
+    FOREIGN KEY (id_vozilo) REFERENCES vozilo (id)
+);
+
+CREATE TABLE kamion (
+    id MEDIUMINT NOT NULL, 
+    id_slika_kamiona INT,
+    id_serija_auto_kamion SMALLINT,
+    id_vozilo INT,
+    duljina NUMERIC (4, 2),
+    visina NUMERIC (3, 2),
+    nosivost NUMERIC (5, 2),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_serija_auto_kamion) REFERENCES serija_auto_kamion (id),
+    FOREIGN KEY (id_vozilo) REFERENCES vozilo (id)
+);
+
+CREATE TABLE serija_auto_kamion (
+    id SMALLINT NOT NULL, 
+    ime VARCHAR (100), 
+    proizvodac VARCHAR (40), 
+    najveca_brzina SMALLINT, 
+    konjska_snaga SMALLINT, 
+    tip_mjenjaca VARCHAR (10), 
+    broj_vrata TINYINT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE serija_motocikl (
+    id SMALLINT NOT NULL,
+    ime VARCHAR (100),
+    proizvodac VARCHAR (40),
+    najveca_brzina SMALLINT,
+    konjska_snaga SMALLINT,
+    broj_sjedala CHAR(1),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE slika_automobila (
+    id INT NOT NULL, 
+    id_automobil MEDIUMINT, 
+    slika VARBINARY(6291456),
+    pozicija VARCHAR (30),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_automobil) REFERENCES automobil (id)
+);
+
+CREATE TABLE slika_kamiona (
+    id INT NOT NULL, 
+    id_kamion MEDIUMINT, 
+    slika VARBINARY(6291456),
+    pozicija VARCHAR (30),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_kamion) REFERENCES kamion (id)
+);
+
+CREATE TABLE slika_motora (
+    id INT NOT NULL, 
+    id_motocikl MEDIUMINT, 
+    slika VARBINARY(6291456),
+    pozicija VARCHAR (30),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_motocikl) REFERENCES motocikl (id)
+);
+
+CREATE TABLE osiguranje (
+    id INT NOT NULL, 
+    id_osiguravacka_kuca MEDIUMINT, 
+    id_vozilo INT, 
+    id_transakcija BIGINT, 
+    datum_pocetka DATE, 
+    datum_zavrsetka DATE, 
+    tip_osiguranja VARCHAR (20),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_osiguravacka_kuca) REFERENCES pravna_osoba (id),
+    FOREIGN KEY (id_vozilo) REFERENCES vozilo (id),
+    FOREIGN KEY (id_transakcija) REFERENCES transakcija (id)
+);
+
+CREATE TABLE steta (
+    id INT NOT NULL, 
+    tip VARCHAR (7),
+    opis TEXT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE naknada_stete (
+    id INT NOT NULL,  
+    datum_pocetka DATE, 
+    datum_zavrsetka DATE, 
+    id_transakcija BIGINT,
+    id_osiguranje INT,
+    id_steta INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_transakcija) REFERENCES transakcija (id),
+    FOREIGN KEY (id_steta) REFERENCES steta (id),
+    FOREIGN KEY (id_osiguranje) REFERENCES osiguranje (id)
+);
+
+CREATE TABLE punjenje (
+    id BIGINT NOT NULL,
+    id_transakcija_punjenje BIGINT,
+    id_vozilo INT,
+    kolicina NUMERIC(7, 3),
+    tip_punjenja VARCHAR (10),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_transakcija_punjenje) REFERENCES transakcija (id),
+    FOREIGN KEY (id_vozilo) REFERENCES vozilo (id)
+);
+
+CREATE TABLE odrzavanje (
+    id BIGINT NOT NULL, 
+    tip_odrzavanja VARCHAR (100), 
+    id_zaposlenik MEDIUMINT, 
+    id_transakcija_odrzavanje BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_zaposlenik) REFERENCES zaposlenik (id),
+    FOREIGN KEY (id_transakcija_odrzavanje) REFERENCES transakcija (id)
+);
+
+CREATE TABLE rezervacija (
+    id BIGINT NOT NULL, 
+    datum_rezervacije DATE, 
+    datum_potvrde DATE, 
+    id_klijent INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_klijent) REFERENCES klijent (id)
+);
+
+CREATE TABLE oprema (
+    id SMALLINT NOT NULL,
+    naziv VARCHAR (30),
+    opis TEXT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE oprema_na_najmu (
+    id BIGINT NOT NULL,
+    id_vozilo_na_najmu BIGINT,
+    id_oprema INT,
+    kolicina TINYINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_vozilo_na_najmu) REFERENCES vozilo_na_najmu (id),
+    FOREIGN KEY (id_oprema) REFERENCES oprema (id)
+);
+
+CREATE TABLE oprema_na_rezervaciji (
+    id BIGINT NOT NULL,
+    id_rezervacija BIGINT,
+    id_oprema INT,
+    kolicina TINYINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_rezervacija) REFERENCES rezervacija (id),
+    FOREIGN KEY (id_oprema) REFERENCES oprema (id)
+);
+
+CREATE TABLE vozilo_na_rezervaciji (
+    id BIGINT NOT NULL,
+    id_rezervacija BIGINT,
+    id_vozilo INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_rezervacija) REFERENCES rezervacija (id),
+    FOREIGN KEY (id_vozilo) REFERENCES vozilo (id)
+);
+
+CREATE TABLE crna_lista (
+    id SMALLINT NOT NULL,
+    id_klijent INT,
+    razlog TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_klijent) REFERENCES klijent (id)
+);
